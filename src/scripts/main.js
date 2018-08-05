6431e26f8e7e5aaf08a0a79bb77e85c2ef56fcef
Joey Biotti <joey@MacBook-Pro-2.local>
2018-08-05 12:21:59 -0500

    added fixnav class and js func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('main.js loaded');
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+function fixNav(){
+    if(window.scrollY){
+        document.body.style.paddingTop = nav.offsetHeight;
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+    }
+}
+
+window.addEventListener('scroll', fixNav);
