ccdbc204a5f485f0693fa2c4a98c4cfb958911d3
Joey Biotti <joey@MacBook-Pro-2.local>
2018-08-04 23:08:44 -0500

    fixed gulp, added functions

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+
+//Copy index.html from src to dist
+gulp.task('copyHtml', function(){
+    gulp.src('./src/*.html')
+        .pipe(gulp.dest('./dist/'));
+});
+
+//Gulp Sass
+gulp.task('sass', function(){
+    gulp.src(['src/sass/*.scss'])
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./dist'));
+});
+
+//Optimize Imgs
+gulp.task('imageMin', function(){
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+})
+
+//Scripts
+gulp.task('scripts', function(){
+    gulp.src('src/scripts/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'));
+});
+
+//Gulp Default
+gulp.task('default', ['copyHtml', 'sass', 'scripts', 'imageMin']);
+
+//Gulp watch
+gulp.task('watch', function(){
+    gulp.watch('src/scripts/*.js', ['scripts']);
+    gulp.watch('src/images/*', ['imageMin']);
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/*.html', ['copyHtml']);
+})
